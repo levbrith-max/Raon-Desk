32fe16586812c9cf8ec7fbfcafc5ca9bb4b6b815
levbrith-max <levbrith@gmail.com>
2026-04-25 23:39:04 +0200

    Add files via upload

---
+++ app/src/main/kotlin/com/gnomebazzite/launcher/ui/store/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.gnomebazzite.launcher.ui.store
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: paramètres du launcher (thème, disposition, apps épinglées…)
+    }
+}
